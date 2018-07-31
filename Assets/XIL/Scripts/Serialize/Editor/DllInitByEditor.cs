8d8a1dc99308d21c73739adc00dcad14e5274c8b
qewsfs <qewsfs@qq.com>
2018-07-31 20:35:06 +0800

    模拟ILMono组件序列化

---
+++ Assets/XIL/Scripts/Serialize/Editor/DllInitByEditor.cs
@@ -0,0 +1,15 @@
+﻿#if USE_HOT
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+[UnityEditor.InitializeOnLoad]
+public class DllInitByEditor
+{
+    static DllInitByEditor()
+    {
+        wxb.hotMgr.Init();
+    }
+}
+#endif
