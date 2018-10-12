f0b0e9977d86aae1e0fbd69372f8018b146e5798
qewsfs <qewsfs@qq.com>
2018-10-13 06:15:33 +0800

    增加说明

--- Hot/HotHelloWorld.cs
+++ Hot/HotHelloWorld.cs
@@ -15,56 +15,10 @@ namespace hot
     [ReplaceType(typeof(HelloWorld))] 
     public static class HotHelloWorld
     {
-        // 替换HelloWorld.TestXX静态方法
-        [ReplaceFunction()]
-        static void TestXX(int p1, int p2, int p3, int p4, string p5, string p6, string p7, string p8, Vector3 p9, Vector2 p10, Vector4 p11)
-        {
-            Debug.LogFormat("p1:{0}", p1);
-            Debug.LogFormat("p2:{0}", p2);
-            Debug.LogFormat("p3:{0}", p3);
-            Debug.LogFormat("p4:{0}", p4);
-            Debug.LogFormat("p5:{0}", p5);
-            Debug.LogFormat("p6:{0}", p6);
-            Debug.LogFormat("p7:{0}", p7);
-            Debug.LogFormat("p8:{0}", p8);
-            Debug.LogFormat("p9:{0}", p9);
-            Debug.LogFormat("p10:{0}", p10);
-            Debug.LogFormat("p11:{0}", p11);
-        }
-
-        public static void Init()
-        {
-            UnityEngine.Debug.LogFormat("HotHelloWorld.Init by AutoInitAndRelease");
-        }
-
-        public static void Reg()
-        {
-            // 手动注册
-            hotMgr.ReplaceFunc(typeof(HelloWorld), "TestFunc", typeof(HotHelloWorld).GetMethod("TestFunc", hotMgr.bindingFlags));
-            hotMgr.ReplaceField(typeof(HelloWorld), "__Hotfix_TestField", typeof(HotHelloWorld).GetMethod("TestField", hotMgr.bindingFlags));
-        }
-
-        static void TestField(HelloWorld world)
-        {
-            UnityEngine.Debug.Log("HotHelloWorld.TestField");
-        }
-
-        static void TestFunc(HelloWorld world)
-        {
-            UnityEngine.Debug.Log("HotHelloWorld.TestFunc");
-        }
-
-        static Hotfix __Hotfix_get_GetValue = null; // 保存回调自身数据
-
-        // 替换接口HelloWorld.getValue属性
-        [ReplaceFunction()]
-        static int get_GetValue(HelloWorld world)
-        {
-            int value = (int)__Hotfix_get_GetValue.Invoke(world); // 调回HelloWorld.getValue自身接口
-            return value + 10000;
-        }
-
-        static Hotfix __Hotfix_Start;
+        // 在实际使用补丁方式热更时，经常遇到一些，只是需要在原有函数之前或之后添加一些代码的情况，
+        // 这时，你可以通过Hotfix来执行原先代码
+        // 类似以下定义的Hotfix字段，会自动赋值
+        static Hotfix __Hotfix_Start; // 保存回调自身数据
 
         // 替换HelloWorld.Start接口
         [ReplaceFunction()]
@@ -86,6 +40,45 @@ namespace hot
             var texts = refType.GetField<List<string>>("onTexts");
             foreach (var ator in texts)
                 UnityEngine.GUILayout.Label(ator);
+        }
+
+        // AutoInitAndRelease属性
+        // 在热更初始化后自动调用
+        public static void Init()
+        {
+            UnityEngine.Debug.LogFormat("HotHelloWorld.Init by AutoInitAndRelease");
+        }
+
+        public static void Reg()
+        {
+            // 手动注册
+            // 替换HelloWorld.TestFunc
+            hotMgr.ReplaceFunc(typeof(HelloWorld), "TestFunc", typeof(HotHelloWorld).GetMethod("TestFunc", hotMgr.bindingFlags));
+
+            // 替换HelloWorld.TestField
+            hotMgr.ReplaceField(typeof(HelloWorld), "__Hotfix_TestField", typeof(HotHelloWorld).GetMethod("TestField", hotMgr.bindingFlags));
+        }
+
+        // 手动注册，替换了HelloWorld.TestFunc
+        static void TestFunc(HelloWorld world)
+        {
+            UnityEngine.Debug.Log("HotHelloWorld.TestFunc");
+        }
+
+        // 手动注册，替换了HelloWorld.TestField
+        static void TestField(HelloWorld world)
+        {
+            UnityEngine.Debug.Log("HotHelloWorld.TestField");
+        }
+
+        static Hotfix __Hotfix_get_GetValue = null; // 保存回调自身数据
+
+        // 替换接口HelloWorld.getValue属性
+        [ReplaceFunction()]
+        static int get_GetValue(HelloWorld world)
+        {
+            int value = (int)__Hotfix_get_GetValue.Invoke(world); // 调回HelloWorld.getValue自身接口
+            return value + 10000;
         }
 
         // 替换HelloWorld.Test接口,带out ref参数
@@ -127,6 +120,23 @@ namespace hot
         static void TestX(HelloWorld world, int x, string name)
         {
             __Hotfix_TestX_3.Invoke(world, x, name);
+        }
+
+        // 替换HelloWorld.TestXX静态方法
+        [ReplaceFunction()]
+        static void TestXX(int p1, int p2, int p3, int p4, string p5, string p6, string p7, string p8, Vector3 p9, Vector2 p10, Vector4 p11)
+        {
+            Debug.LogFormat("p1:{0}", p1);
+            Debug.LogFormat("p2:{0}", p2);
+            Debug.LogFormat("p3:{0}", p3);
+            Debug.LogFormat("p4:{0}", p4);
+            Debug.LogFormat("p5:{0}", p5);
+            Debug.LogFormat("p6:{0}", p6);
+            Debug.LogFormat("p7:{0}", p7);
+            Debug.LogFormat("p8:{0}", p8);
+            Debug.LogFormat("p9:{0}", p9);
+            Debug.LogFormat("p10:{0}", p10);
+            Debug.LogFormat("p11:{0}", p11);
         }
     }
 
